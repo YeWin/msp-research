fe8ceea34c2cf6b4b6d36b10a38d6b41888c2848
yewin <techfun@NB0029>
2017-05-17 16:12:15 +0630

    config change for extra plugins.

--- MSP_Research/WebContent/resources/js/ckeditor.js
+++ MSP_Research/WebContent/resources/js/ckeditor.js
@@ -1,6 +1,10 @@
-CKEDITOR.editorConfig = function(config) {
- var contextPath = $('.contextPath').html();
-	 /*config.filebrowserBrowseUrl = contextPath+'/ckeditor/browseimage';
+CKEDITOR.editorConfig = function(config) { 
+	
+	 /*
+	  * var contextPath = $('.contextPath').html();
+	  * config.filebrowserBrowseUrl = contextPath+'/ckeditor/browseimage';
 	 config.filebrowserImageUploadUrl = contextPath+'/ckeditor/uploadimage';
 	 alert("config.allowedContent: " + config.allowedContent);*/
+	
+	config.extraPlugins = 'image2,widget,widgetselection,clipboard,lineutils,codesnippet';
 };
